d3537e85979b671cd2223fe5632c7fd0c32c6dcb
decentninja <encrypt.wave@gmail.com>
2019-06-01 20:19:08 +0200

    Removed cross compilation stuff

--- install-openssl-cross.bash
+++
@@ -1,6 +0,0 @@
-wget https://www.openssl.org/source/openssl-1.0.1t.tar.gz
-tar xzf openssl-1.0.1t.tar.gz
-export MACHINE=armv7
-export ARCH=arm
-export CC=arm-linux-gnueabihf-gcc
-cd openssl-1.0.1t && ./config shared && make && cd -
